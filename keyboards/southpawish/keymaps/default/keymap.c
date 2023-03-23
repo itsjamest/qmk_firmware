e349b2259b6d68fb0c60d4f4469da1fceae67231
Jimbojib <42556180+Jimbojib@users.noreply.github.com>
2023-03-23 09:22:06 +0000

    keymap changes

--- keyboards/southpawish/keymaps/default/keymap.c
+++ keyboards/southpawish/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-// Copyright 2023 %YOUR_FULL_NAME% (@%YOUR_GITHUB_USERNAME%)
+// Copyright  %YOUR_FULL_NAME% (@%YOUR_GITHUB_USERNAME%)
 // SPDX-License-Identifier: GPL-2.0-or-later
 //
 #include QMK_KEYBOARD_H
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_TOG,    KC_2,    KC_PGUP, KC_HOME, KC_INS,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         RGB_MOD,    RGB_RMOD,    KC_PGDN, KC_END,  KC_DEL,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_ENT,
         KC_5,    KC_6,    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_HASH,
-        KC_7,    KC_8,    KC_UP,   KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
+        C(LALT(S)),    KC_8,    KC_UP,   KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
         LALT(KC_COMMA),    LALT(KC_DOT),    KC_LEFT, KC_DOWN, KC_RGHT, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_RALT, KC_LGUI, KC_MENU, KC_LCTL
     ),
     
@@ -29,7 +29,7 @@ void housekeeping_task_user(void) {
     uint8_t red = led_state.caps_lock ? 75 : 0;
     uint8_t green = layer_state_is(0) ? 75 : 0;
     uint8_t blue = layer_state_is(1) ? 0 : 0;
-    rgblight_setrgb_range(red, green, blue, 3,5);
+    rgblight_setrgb_range(red, green, blue, 0,1);
 }
 
 //void housekeeping_task_user(void) {
